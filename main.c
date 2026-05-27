3d1f0332cb4c119e531d2cb984a46db0ac946727
Amith E K <amithajith7025@gmail.com>
2026-05-27 07:08:13 +0530

    completed.

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "stb_image_write.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 #define RED   0
 #define GREEN 1
@@ -12,10 +14,17 @@
 
 int width, height, channels;
 
+int GX[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+int GY[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+
+
 
 unsigned char mat_get(unsigned char*, int, int, int);
 void mat_set(unsigned char*, int, int, int, unsigned char);
 unsigned char *convert_grey_scale(unsigned char*);
+int element_wise_multiplication(unsigned char*, int x, int y, int[3][3]);
+unsigned char *sobel_technique(unsigned char*);
+
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -52,7 +61,15 @@ int main(int argc, char **argv) {
         grey_scale,
         width
     );
-
+    unsigned char *sketch = sobel_technique(grey_scale);
+    stbi_write_png(
+        "output/sketch.png",
+        width,
+        height,
+        1,
+        sketch,
+        width
+    );
 
     return 0;
 }
@@ -83,4 +100,36 @@ unsigned char* convert_grey_scale(unsigned char* data) {
         }
     }
     return grey_scale;
+}
+
+int element_wise_multiplication(unsigned char *grey_scale, int x, int y, int GX[3][3]) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            int neighbour_x = x + j - 1;
+            int neighbour_y = y + i - 1;
+            int mat_value = grey_scale[neighbour_y * width + neighbour_x];
+            sum += mat_value * GX[i][j];
+        }
+    }
+    return sum;
+}
+
+unsigned char *sobel_technique(unsigned char *grey_scale) {
+    unsigned char *sketch = (unsigned char *) malloc(width * height);
+    memset(sketch, 0, width * height);
+    for (int i = 1; i < height - 1; i++) {
+        for (int j = 1; j < width - 1; j++) {
+            int gx = element_wise_multiplication(grey_scale, j, i, GX);
+            int gy = element_wise_multiplication(grey_scale, j, i, GY);
+            double g = sqrt(
+                        (double)(gx * gx) +
+                        (double)(gy * gy)
+                    );
+            if (g > 255)
+                g = 255;
+            sketch[i * width + j] = (unsigned char)g;
+        }
+    }
+    return sketch;
 }
